9214a5837579cffee67bd58c67230ec55606beed
Owen Ayres <owen.ayres@gmail.com>
2018-04-20 17:12:15 +0100

    Fixes template

--- src/server/template.js
+++ src/server/template.js
@@ -1,39 +1,12 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import Helmet from 'react-helmet'
 import pathPrefix from '../constants/pathPrefix'
 
 class Html extends Component {
-  renderTealium (isNotLocal) {
-    if (isNotLocal) {
-      const tealium = process.env.TEALIUM || '//tags.tiqcdn.com/utag/vodafone/uk-main/dev/utag.sync.js'
-      return <script src={tealium} />
-    }
-  }
-
-  renderSnare (isNotLocal) {
-    if (isNotLocal) {
-      return (
-        <Fragment>
-          <input type='hidden' id='ioBlackBox' />
-          <script src='https://mpsnare.iesnare.com/snare.js' />
-        </Fragment>
-      )
-    }
-  }
-
   render () {
     const state = this.props.state
-    const isNotLocal = process.env.NODE_ENV !== 'local'
-    const injectedState = `window.__STATE = ${JSON.stringify(state, null, isNotLocal ? 0 : 4)};`
+    const injectedState = `window.__STATE = ${JSON.stringify(state, null, 4)};`
     const helmet = Helmet.renderStatic()
-    const globals = `var dalmatianPrefix = '${pathPrefix}';`
-    const iovationContent = isNotLocal ? `
-      var io_bbout_element_id = 'ioBlackBox';
-      var io_install_stm = false;
-      var io_exclude_stm = 12;
-      var io_install_flash = false;
-      var io_enable_rip = true;
-    ` : ''
 
     return (
       <html lang='en-gb' {...helmet.htmlAttributes.toComponent()}>
@@ -49,17 +22,9 @@ class Html extends Component {
           <link href={`${pathPrefix}/assets/styles.css`} rel='stylesheet' />
           <link rel='icon' type='image/x-icon' href='/favicon.ico' />
           {helmet.link.toComponent()}
-
-          {/* Globals, must come first */}
-          <script dangerouslySetInnerHTML={{__html: globals}} />
-
-          {/* Tealium init */}
-          {this.renderTealium(isNotLocal)}
         </head>
         <body {...helmet.bodyAttributes.toComponent()}>
           <div id='root' dangerouslySetInnerHTML={{__html: this.props.children}} />
-          {this.renderSnare(isNotLocal)}
-          <script dangerouslySetInnerHTML={{__html: iovationContent}} />
           <script dangerouslySetInnerHTML={{__html: injectedState}} />
           <script src={`${pathPrefix}/assets/sw.js`} />
           <script src={`${pathPrefix}/assets/bundle.js`} />
